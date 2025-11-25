7d3d0106fc6d6de6962aaa244e7fa37dff5a0f2c
MUHUMUZAJeanPierre <muhumuzajeanpierre9@gmail.com>
2025-11-25 18:09:01 +0200

    feat

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,10 +1,15 @@
+import car from "../assets/car.png"
+import ticks from "../assets/ticks.png"
+import headset from "../assets/headset.png"
 const Features: React.FC = () => {
   return (
     <div className="flex justify-evenly flex-wrap gap-10 py-10 text-green-900 font-medium">
 
       {/* Free Shipping */}
       <div className="flex items-center text-center gap-3">
-        <p className="text-4xl">🚚</p>
+        <div className="w-12 h-12 flex-shrink-0">
+          <img src={car} alt="car" className="w-full h-full object-cover" />
+        </div>
         <div className="text-left">
           <h3 className="font-semibold">Free Shipping</h3>
           <p className="text-sm text-gray-600">On all orders over $0</p>
@@ -13,7 +18,9 @@ const Features: React.FC = () => {
 
       {/* Secure Payments */}
       <div className="flex items-center text-center gap-3">
-        <p className="text-4xl">🔒</p>
+        <div className="w-12 h-12 flex-shrink-0">
+          <img src={ticks} alt="ticks" className="w-full h-full object-cover" />
+        </div>
         <div className="text-left">
           <h3 className="font-semibold">Secure Payments</h3>
           <p className="text-sm text-gray-600">100% safe and encrypted</p>
@@ -22,7 +29,10 @@ const Features: React.FC = () => {
 
       {/* 24/7 Support */}
       <div className="flex items-center text-center gap-3">
-        <p className="text-4xl">🎧</p>
+        <div className="w-12 h-12 flex-shrink-0">
+          <img src={headset} alt="headset" className="w-full h-full object-cover" />
+        </div>
+
         <div className="text-left">
           <h3 className="font-semibold">24/7 Support</h3>
           <p className="text-sm text-gray-600">We’re here anytime</p>
